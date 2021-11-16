206fea4fb703264d49f96186a13f40846c9817f6
EC2 Default User <ec2-user@ip-172-31-86-207.ec2.internal>
2021-11-16 05:29:58 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def index
     @users = User.all
+    @user = current_user
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
- if @user.update(user_params)
-    flash[:notice]="You have updated user successfully."
+  if @user.update(user_params)
     redirect_to user_path(@user.id)
-   else
+  else
       render :edit
   end
   end
-  
-  private 
+
+  private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
