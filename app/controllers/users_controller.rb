572d6b982a9724e264381e964ce7500928ab20fe
Seren <itoseren@gmail.com>
2022-01-31 14:54:16 +0000

    [add]ゲストユーザー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only:[:edit]
+  
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -13,11 +16,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
- if @user == current_user
+    if @user == current_user
    render :edit
- else
-   redirect_to user_path(current_user)
-  end
+    else
+    redirect_to user_path(current_user)
+    end
   end
 
   def update
@@ -33,4 +36,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user),notice: 'ゲストユーザーはプロフィール画面へ遷移できません。'
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   attachment :profile_image
   has_many :books, dependent: :destroy
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
-  
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser',email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 end
